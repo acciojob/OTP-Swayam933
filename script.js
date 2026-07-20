2d5c01e979180057fe147eea77eaf2f6e08b60ff
Chandan Kumar Pradhan <swayampradhan24@gmail.com>
2026-07-20 22:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+// Focus first input
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  // Move forward when typing
+  code.addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/\D/g, "");
+
+    if (e.target.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  // Handle backspace
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (code.value === "" && index > 0) {
+        codes[index - 1].value = "";
+        codes[index - 1].focus();
+      } else {
+        code.value = "";
+      }
+    }
+  });
+});
