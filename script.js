30ae123a1be882024ca11ae8f5c13df11e03f1f6
Chandan Kumar Pradhan <swayampradhan24@gmail.com>
2026-07-20 22:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 const codes = document.querySelectorAll(".code");
 
-// Focus first input
-codes[0].focus();
+// Focus the first input
+window.onload = () => {
+  codes[0].focus();
+};
 
 codes.forEach((code, index) => {
-  // Move forward when typing
   code.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/\D/g, "");
+    // Allow only one digit
+    e.target.value = e.target.value.replace(/[^0-9]/g, "").slice(0, 1);
 
+    // Move to next input
     if (e.target.value && index < codes.length - 1) {
       codes[index + 1].focus();
     }
   });
 
-  // Handle backspace
   code.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      if (code.value === "" && index > 0) {
+      e.preventDefault();
+      code.value = "";
+
+      if (index > 0) {
         codes[index - 1].value = "";
         codes[index - 1].focus();
-      } else {
-        code.value = "";
       }
     }
   });
